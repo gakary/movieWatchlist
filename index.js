dd983800767f13f6c9e6ad3223a9bd20b2148530
garyfung221 <garyfung266@gmail.com>
2023-03-18 11:29:29 +0800

    format the code

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const key = "c31e8d0f";
 const moviesTitle = [];
-const searchBtn = document.getElementById('search-btn');
-const getSearch = document.getElementById('search-input');
-const movieDiv = document.getElementById('hold');
-let watchList = []
+const searchBtn = document.getElementById("search-btn");
+const getSearch = document.getElementById("search-input");
+const movieDiv = document.getElementById("hold");
+let watchList = [];
 
 document.addEventListener("click", (e) => {
-    //only work for element have the data-movie
+  //only work for element have the data-movie
   if (e.target.dataset.movie) {
-       //catch the whole element belong to this data-movie
+    //catch the whole element belong to this data-movie
     const button = e.target;
 
     if (button.src.includes("images/add.png")) {
@@ -21,72 +21,64 @@ document.addEventListener("click", (e) => {
   }
 });
 
-
-handleAddWatchList = (movie) =>{
-    //the movie is string , if want to store into localStorage , it have to convert to JSON
-    let watchListData = localStorage.getItem("WatchListData");
-    if (watchListData) {
-        // if watchListData exists in localStorage, parse it and append new movie
-        watchList = JSON.parse(watchListData);
-        watchList.push(movie);
-    } else {
-        // if watchListData does not exist in localStorage, initialize watchList with new movie
-        watchList = [movie];
-    }
-    localStorage.setItem("WatchListData", JSON.stringify(watchList));
-}
-handleRemoveWatchList = (movie) =>{
-    console.log("its :"+typeof movie)
-    watchList = watchList.filter((movieObject)=>{
-        /*because when JSON string are pushed into watchList array
+handleAddWatchList = (movie) => {
+  //the movie is string , if want to store into localStorage , it have to convert to JSON
+  let watchListData = localStorage.getItem("WatchListData");
+  if (watchListData) {
+    // if watchListData exists in localStorage, parse it and append new movie
+    watchList = JSON.parse(watchListData);
+    watchList.push(movie);
+  } else {
+    // if watchListData does not exist in localStorage, initialize watchList with new movie
+    watchList = [movie];
+  }
+  localStorage.setItem("WatchListData", JSON.stringify(watchList));
+};
+handleRemoveWatchList = (movie) => {
+  console.log("its :" + typeof movie);
+  watchList = watchList.filter((movieObject) => {
+    /*because when JSON string are pushed into watchList array
         this array inside all string are JSON string ,
         so that we have to using JSON.stringify to convert to json string
         to checking return by filter movieObject is that equal*/
-        return movieObject.Title !== movie.Title 
+    return movieObject.Title !== movie.Title;
+  });
+  localStorage.setItem("WatchListData", JSON.stringify(watchList));
+};
+
+searchBtn.addEventListener("click", () => {
+  //get user searching value & remove the spacing between
+  let searchText = getSearch.value.trim();
+  console.log(searchText);
+
+  //push first 10 movies into array
+  addTitleList(searchText)
+    .then(() => {
+      // added .then() block to wait for addTitleList() to complete
+      getMoviesDetails(moviesTitle);
     })
-    localStorage.setItem("WatchListData", JSON.stringify(watchList));
-}
-
-
-
-
-
-
-
-
-
-searchBtn.addEventListener("click",()=>{
-    //get user searching value & remove the spacing between
-    let searchText = getSearch.value.trim();
-    console.log(searchText);
-    
-    //push first 10 movies into array
-    addTitleList(searchText)
-        .then(() => {  // added .then() block to wait for addTitleList() to complete
-            getMoviesDetails(moviesTitle);
-        })
-        .then()
-})
-
-
+    .then();
+});
 
 const addTitleList = async (searchText) => {
-    /* api provide a http , but it have to use https  */
-    const response = await fetch(`https://www.omdbapi.com/?s=${searchText}&apikey=${key}`);
-    const data = await response.json();
-    for (let movie of data.Search) {
-        moviesTitle.push(movie.Title);
-    }
-}
+  /* api provide a http , but it have to use https  */
+  const response = await fetch(
+    `https://www.omdbapi.com/?s=${searchText}&apikey=${key}`
+  );
+  const data = await response.json();
+  for (let movie of data.Search) {
+    moviesTitle.push(movie.Title);
+  }
+};
 const getMoviesDetails = (array) => {
-    console.log(typeof array)
-    let html = ""
-    for (let i = 0; i < array.length; i++) {
-        console.log(array[i])
-        fetch(`https://www.omdbapi.com/?t=${array[i]}&apikey=${key}`)
-            .then(res => res.json())
-            .then(data => {
-                html += `<div class="result-container">
+  console.log(typeof array);
+  let html = "";
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+    fetch(`https://www.omdbapi.com/?t=${array[i]}&apikey=${key}`)
+      .then((res) => res.json())
+      .then((data) => {
+        html += `<div class="result-container">
                     <img class="mov-image" src="${data.Poster}">
                     <div class="mov-details">
                         <div class="mov-header">
@@ -105,16 +97,12 @@ const getMoviesDetails = (array) => {
                         </div>
                     </div>
                 </div>
-                <hr>`
-                render(html);
-            }) //end then
-    } //end for
-    
-}
+                <hr>`;
+        render(html);
+      }); //end then
+  } //end for
+};
 
-
-const render = (html) =>{
-    movieDiv.innerHTML = html
-}
-
-
+const render = (html) => {
+  movieDiv.innerHTML = html;
+};
--- watchlist.js
+++ watchlist.js
@@ -1,23 +1,16 @@
- 
 const key = "c31e8d0f";
-const watchDiv = document.getElementById('watchlist_hold')
-let watchList = []
-const alertDiv = document.getElementById('alert')
-
- 
-
-
+const watchDiv = document.getElementById("watchlist_hold");
+let watchList = [];
+const alertDiv = document.getElementById("alert");
 
 document.addEventListener("click", (e) => {
   if (e.target.dataset.movie) {
-
     const button = e.target;
 
     if (button.src.includes("images/add.png")) {
       handleAddWatchList(button.dataset.movie);
       button.src = "images/added.png";
     } else {
-
       // call handleRemoveWatchList function and pass the movie title as a parameter
       handleRemoveWatchList(button.dataset.movie);
       button.src = "images/add.png";
@@ -25,20 +18,19 @@ document.addEventListener("click", (e) => {
   }
 });
 
-
-handleAddWatchList = (movie) =>{
-    //the movie is string , if want to store into localStorage , it have to convert to JSON
-    let watchListData = localStorage.getItem("WatchListData");
-    if (watchListData) {
-        // if watchListData exists in localStorage, parse it and append new movie
-        watchList = JSON.parse(watchListData);
-        watchList.push(movie);
-    } else {
-        // if watchListData does not exist in localStorage, initialize watchList with new movie
-        watchList = [movie];
-    }
-    localStorage.setItem("WatchListData", JSON.stringify(watchList));
-}
+handleAddWatchList = (movie) => {
+  //the movie is string , if want to store into localStorage , it have to convert to JSON
+  let watchListData = localStorage.getItem("WatchListData");
+  if (watchListData) {
+    // if watchListData exists in localStorage, parse it and append new movie
+    watchList = JSON.parse(watchListData);
+    watchList.push(movie);
+  } else {
+    // if watchListData does not exist in localStorage, initialize watchList with new movie
+    watchList = [movie];
+  }
+  localStorage.setItem("WatchListData", JSON.stringify(watchList));
+};
 
 const handleRemoveWatchList = (movieTitle) => {
   // Retrieve the watchList data from localStorage
@@ -46,25 +38,23 @@ const handleRemoveWatchList = (movieTitle) => {
 
   if (watchListData) {
     // Parse the watchList data and filter out the movie with the given title
-    watchList = JSON.parse(watchListData).filter((movie) => movie !== movieTitle);
+    watchList = JSON.parse(watchListData).filter(
+      (movie) => movie !== movieTitle
+    );
 
     // Store the updated watchList data back into localStorage
     localStorage.setItem("WatchListData", JSON.stringify(watchList));
   }
 };
 
-
-
-
-     
 const getWatchListMoviesDetails = (array) => {
-    let html = ""
-    for (let i = 0; i < array.length; i++) {
-        console.log(array[i])
-        fetch(`https://www.omdbapi.com/?t=${array[i]}&apikey=${key}`)
-            .then(res => res.json())
-            .then(data => {
-                html += `<div class="result-container">
+  let html = "";
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+    fetch(`https://www.omdbapi.com/?t=${array[i]}&apikey=${key}`)
+      .then((res) => res.json())
+      .then((data) => {
+        html += `<div class="result-container">
                     <img class="mov-image" src="${data.Poster}">
                     <div class="mov-details">
                         <div class="mov-header">
@@ -83,18 +73,17 @@ const getWatchListMoviesDetails = (array) => {
                         </div>
                     </div>
                 </div>
-                <hr>`
-                 
-                renderWatchList(html);
-            }) //end then
-    } //end for
-}
+                <hr>`;
 
-   const myLocalStorageData = localStorage.getItem("WatchListData")
-     const myList = JSON.parse(myLocalStorageData)
-     getWatchListMoviesDetails(myList);
+        renderWatchList(html);
+      }); //end then
+  } //end for
+};
 
-const renderWatchList = (html) =>{
-    watchDiv.innerHTML = html
-    console.log("2")
-}
+const myLocalStorageData = localStorage.getItem("WatchListData");
+const myList = JSON.parse(myLocalStorageData);
+getWatchListMoviesDetails(myList);
+
+const renderWatchList = (html) => {
+  watchDiv.innerHTML = html;
+};
